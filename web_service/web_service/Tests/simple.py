b30c06356f0d103b0707e4acb6eb777d2b7dd074
m4gik <michal.szczygiel@wp.pl>
2015-01-10 01:42:48 +0100

    Create simply test

---
+++ web_service/web_service/Tests/simple.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# @COPYRIGHT_begin
+#
+# Copyright [2015] Michał Szczygieł, M4GiK Software
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @COPYRIGHT_end
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+
+import unittest
+
+
+class TestTeamcityMessages(unittest.TestCase):
+    def testPass(self):
+        assert 1 == 1
+
+    def testAssertEqual(self):
+        self.assertEqual("1", "2")
+
+    def testAssertEqualMsg(self):
+        self.assertEqual("1", "2", "message")
+
+    def testAssert(self):
+        self.assert_(False)
+
+    def testFail(self):
+        self.fail("failed")
+
+    def testException(self):
+        raise Exception("some exception")
+
+
+if __name__ == '__main__':
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
