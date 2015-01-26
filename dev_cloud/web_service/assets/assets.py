2e1f92dd18e3839055558ea3803983f484e6f4df
m4gik <michal.szczygiel@wp.pl>
2015-01-26 10:05:59 +0100

    Add template

--- dev_cloud/web_service/assets/assets.py
+++ dev_cloud/web_service/assets/assets.py
@@ -18,7 +18,7 @@
 # @COPYRIGHT_end
 
 from django_assets import Bundle, register
-
+# ----------------------------------------------------------------------------------------------------------------------
 js = Bundle('../assets/dist/scripts/vendor.js',
             '../assets/dist/scripts/ui.js',
             '../assets/dist/scripts/app.js', output='gen/packed.js')
@@ -28,3 +28,31 @@ css = Bundle('../assets/dist/bower_components/font-awesome/css/font-awesome.min.
              '../assets/dist/bower_components/weather-icons/css/weather-icons.min.css',
              '../assets/dist/styles/main.css', output='gen/packed.css')
 register('template_css', css)
+# ----------------------------------------------------------------------------------------------------------------------
+main_js = Bundle('../assets/main/js/vendor/modernizr.js',
+                 '../assets/main/js/vendor/jquery.js',
+                 '../assets/main/js/foundation.min.js',
+                 '../assets/main/js/vendor/hoverIntent.js',
+                 '../assets/main/js/vendor/superfish.min.js',
+                 '../assets/main/js/vendor/morphext.min.js',
+                 '../assets/main/js/vendor/wow.min.js',
+                 '../assets/main/js/vendor/jquery.slicknav.min.js',
+                 '../assets/main/js/vendor/waypoints.min.js',
+                 '../assets/main/js/vendor/jquery.animateNumber.min.js',
+                 '../assets/main/js/vendor/owl.carousel.min.js',
+                 '../assets/main/js/vendor/jquery.slicknav.min.js',
+                 '../assets/main/js/custom.js', output='gen/packed_main.js')
+register('template_main_js', main_js)
+
+main_css = Bundle('../assets/main/css/normalize.css',
+                  '../assets/main/css/foundation.css',
+                  '../assets/main/css/font-awesome.min.css',
+                  '../assets/main/css/animate.min.css',
+                  '../assets/main/css/morphext.css',
+                  '../assets/main/css/owl.carousel.css',
+                  '../assets/main/css/owl.theme.css',
+                  '../assets/main/css/owl.transitions.css',
+                  '../assets/main/css/slicknav.css',
+                  '../assets/main/style.css', output='gen/packed_main.css')
+register('template_main_css', main_css)
+# ----------------------------------------------------------------------------------------------------------------------
