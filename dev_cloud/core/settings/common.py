7ca6fdfa7826d440788a8c6b07f52d274a93ad04
m4gik <michal.szczygiel@wp.pl>
2015-02-07 10:04:37 +0100

    #24 Change static url from assets to static

--- dev_cloud/core/settings/common.py
+++ dev_cloud/core/settings/common.py
@@ -160,7 +160,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = normpath(join(DJANGO_ROOT, 'static'))
 
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
