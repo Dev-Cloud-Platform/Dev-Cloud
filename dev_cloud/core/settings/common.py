5f3e91e72d382a617c91ada318bdd7c297e648ac
m4gik <michal.szczygiel@wp.pl>
2015-05-18 22:32:36 +0200

    #7 #42 Fix celery imports

--- dev_cloud/core/settings/common.py
+++ dev_cloud/core/settings/common.py
@@ -382,7 +382,7 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 
-CELERY_IMPORTS = ('request',)
+CELERY_IMPORTS = ('tasks',)
 
 # See: http://celery.github.com/celery/django/
 setup_loader()
