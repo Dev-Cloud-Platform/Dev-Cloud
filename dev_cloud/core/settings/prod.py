686ec363e0c4f66ff4b2a076b8d2e53b0c3577dc
m4gik <michal.szczygiel@wp.pl>
2015-06-06 17:02:42 +0200

    #6 Add additional data to ALLOWED_HOSTS

--- dev_cloud/core/settings/prod.py
+++ dev_cloud/core/settings/prod.py
@@ -173,7 +173,7 @@ SECRET_KEY = environ.get('SECRET_KEY', SECRET_KEY)
 
 ########## ALLOWED HOSTS CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['.192.245.169.169']
+ALLOWED_HOSTS = ['.192.245.169.169', '.127.0.0.1']
 ########## END ALLOWED HOST CONFIGURATION
 
 
