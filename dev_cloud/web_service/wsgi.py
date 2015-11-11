a89c7878d9becd668f10e0d8840accb4fba77536
m4gik <michal.szczygiel@wp.pl>
2015-11-11 21:52:37 +0100

    Change configuration for wsgi settings and common

--- dev_cloud/web_service/wsgi.py
+++ dev_cloud/web_service/wsgi.py
@@ -24,10 +24,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-from core.settings.config import ENVIROMENT_PATH
-
-activate_this = ENVIROMENT_PATH
-execfile(activate_this, dict(__file__=activate_this))
+# from core.settings.config import ENVIROMENT_PATH
+#
+# activate_this = ENVIROMENT_PATH
+# execfile(activate_this, dict(__file__=activate_this))
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
