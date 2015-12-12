c31a3f589e172dc28841cbcaf588f8bead29c30d
m4gik <michal.szczygiel@wp.pl>
2015-12-12 19:57:19 +0100

    #47 Delete wrong import

--- dev_cloud/web_service/urls/user/environment.py
+++ dev_cloud/web_service/urls/user/environment.py
@@ -20,8 +20,7 @@ from django.conf.urls import patterns, url, include
 from core.utils.decorators import user_permission, vm_permission
 from web_service.views.user.enviroment import wizard_setup, generate_dependencies, customize_environment, \
     define_environment, summary, validation_process, validation_process_ip, validation_process_resources, \
-    validation_process_ip_pre, view_environment, environments_list, get_vm_status, destroy_vm, refresh_vm_tasks, \
-    force_destroy_vm
+    validation_process_ip_pre, view_environment, environments_list, get_vm_status, destroy_vm, refresh_vm_tasks
 
 main_patterns = patterns('web_service.views.user.enviroment',
                          url(r'^app/create/environment/$', user_permission(wizard_setup),
